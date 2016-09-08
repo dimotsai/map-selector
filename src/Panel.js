76a928d6fb8106391d3387a6136e305343d9258a
Chih-Hao Tsai <a968574123@hotmail.com>
2016-09-09 05:55:28 +0800

    Now support copying to clipboard

--- src/Panel.js
+++ src/Panel.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {List, ListItem} from 'material-ui/List';
+import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
+import IconButton from 'material-ui/IconButton';
+import Divider from 'material-ui/Divider';
+import ActionDelete from 'material-ui/svg-icons/action/delete';
+import Snackbar from 'material-ui/Snackbar';
 import numeral from 'numeral';
 
+
 export default class Panel extends React.Component {
+
+    state = {
+        snackbarOpen: false
+    }
 
     constructor(props) {
         super(props);
@@ -16,14 +30,52 @@ export default class Panel extends React.Component {
         let neLng = numeral(ne.lng()).format(format);
         let swLat = numeral(sw.lat()).format(format);
         let swLng = numeral(sw.lng()).format(format);
-        return `(${neLat},${neLng}),(${swLat},${swLng})`;
+        return {neLat, neLng, swLat, swLng};
+    }
+
+    renderList() {
+        let items = [];
+        this.props.rectangles.forEach(function(r, idx, arr) {
+            let bounds = this.formatBounds(r.getBounds());
+            let text = `${bounds.neLat}, ${bounds.neLat} → ${bounds.swLat}, ${bounds.swLng}`;
+            items.push(
+                <CopyToClipboard text={JSON.stringify(bounds, null, 2)}>
+                    <ListItem key={idx}
+                      rightIconButton={<IconButton><ActionDelete/></IconButton>}
+                      primaryText={text}
+                      onTouchTap={::this.handleSnackbarOpen}
+                    />
+                </CopyToClipboard>
+            );
+            if (idx != arr.length - 1) {
+                items.push(<Divider/>);
+            }
+        }.bind(this));
+        return <List> {items} </List>;
+    }
+
+    handleSnackbarOpen() {
+        this.setState({ snackbarOpen: true });
+    }
+
+    handleSnackbarClose() {
+        this.setState({ snackbarOpen: false });
     }
 
     render() {
-        return <div id="panel">
-                <ol>
-                    {this.props.rectangles.map( (r, index) => <li key={index}>{this.formatBounds(r.getBounds())}</li> )}
-                </ol>
+        return (
+            <div id="panel">
+              <MuiThemeProvider muiTheme={getMuiTheme()}>
+                <div>
+                    {this.renderList()}
+                    <Snackbar
+                     open={this.state.snackbarOpen}
+                     message="已複製到剪貼簿"
+                     autoHideDuration={2000}
+                     onRequestClose={::this.handleSnackbarClose}
+                    />
+                </div>
+              </MuiThemeProvider>
             </div>
-    }
+        )}
 }
